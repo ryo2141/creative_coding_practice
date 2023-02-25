88a4a9d9113a41ee8d4c880c4edb4fc1e06aab72
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-02-25 15:04:11 +0900

    マウス座標利用

--- app03.js
+++ app03.js
@@ -1,14 +1,19 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     
-    strokeWeight(10);
-    stroke(240); // 色番号 0~255
-    noFill(); // 塗りなし
-    // 幅 / 4なので、横25%の位置に円を表示
-    // circle(x座標, y座標, 直径)
-    circle(width / 4, height / 2, 100);
+    // strokeWeight(10);
+    // stroke(240); // 色番号 0~255
+    // noFill(); // 塗りなし
+    // // 幅 / 4なので、横25%の位置に円を表示
+    // // circle(x座標, y座標, 直径)
+    // circle(width / 4, height / 2, 100);
 
-    noStroke();
-    fill(240);
-    circle(width / 4 * 3, height / 2, 100);
+    // noStroke();
+    // fill(240);
+    // circle(width / 4 * 3, height / 2, 100);
+}
+
+function draw() {
+    clear();
+    circle(mouseX, mouseY, 100);
 }
