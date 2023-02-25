c0a1957eb72e9ba81052dc864ced33e78fd32fce
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-02-25 15:10:43 +0900

    ランダム利用その２

--- app03.js
+++ app03.js
@@ -3,7 +3,7 @@ function setup() {
     
     // strokeWeight(10);
     // stroke(240); // 色番号 0~255
-    // noFill(); // 塗りなし
+    noFill(); // 塗りなし
     // // 幅 / 4なので、横25%の位置に円を表示
     // // circle(x座標, y座標, 直径)
     // circle(width / 4, height / 2, 100);
@@ -19,5 +19,12 @@ function draw() {
     // circle(mouseX, mouseY, 100);
     // circle(mouseY, mouseX, 100);
     // 直径をランダムにする
-    circle(mouseX, mouseY, random(100));
+    // circle(mouseX, mouseY, random(100));
+
+    // ストロークの太さを1~5にする
+    strokeWeight(random(1, 5));
+    // ストロークの色を180~255にする
+    stroke(random(180, 255));
+    // 直径を50~300にする
+    circle(width / 2, height / 2, random(50, 300));
 }
