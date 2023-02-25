28dccd5400a89b0990f7d4238b53f7ea155c776e
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-02-25 15:13:42 +0900

    楕円の作例１

--- app03.js
+++ app03.js
@@ -1,6 +1,7 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     
+    // ellipse(width / 2, height / 2, width, height);
     // strokeWeight(10);
     // stroke(240); // 色番号 0~255
     noFill(); // 塗りなし
@@ -22,9 +23,11 @@ function draw() {
     // circle(mouseX, mouseY, random(100));
 
     // ストロークの太さを1~5にする
-    strokeWeight(random(1, 5));
+    // strokeWeight(random(1, 5));
     // ストロークの色を180~255にする
     stroke(random(180, 255));
     // 直径を50~300にする
-    circle(width / 2, height / 2, random(50, 300));
+    // circle(width / 2, height / 2, random(50, 300));
+    // 楕円の横幅、縦幅をランダム
+    ellipse(width / 2, height / 2, random(50, 300), random(50, 300));
 }
