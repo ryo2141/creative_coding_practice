dd410d83b2b73f31f66b1e1ac492ee2310f0e444
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-02-25 14:55:46 +0900

    塗とストローク02

--- app03.js
+++ app03.js
@@ -2,8 +2,13 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     
     strokeWeight(10);
-    stroke(100); // 色番号 0~255
-    fill(240);
+    stroke(240); // 色番号 0~255
+    noFill(); // 塗りなし
+    // 幅 / 4なので、横25%の位置に円を表示
     // circle(x座標, y座標, 直径)
-    circle(width / 2, height / 2, 200);
+    circle(width / 4, height / 2, 100);
+
+    noStroke();
+    fill(240);
+    circle(width / 4 * 3, height / 2, 100);
 }
