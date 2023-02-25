be67964c344452c8ff916828a6865c0439d39bb7
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-02-25 14:34:44 +0900

    fill stroke

--- app03.js
+++ app03.js
@@ -1,6 +1,9 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     
+    strokeWeight(10);
+    stroke(100);
+    fill(240);
     // circle(x座標, y座標, 直径)
     circle(width / 2, height / 2, 200);
 }
