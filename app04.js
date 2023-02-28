c53032fd4a0bb09e70eb7589d87aa5887c8fc78a
ryo2141 <ryo.miyauchi.2703.19@gmail.com>
2023-03-01 01:57:00 +0900

    半径を持つ

--- app04.js
+++ app04.js
@@ -1,4 +1,5 @@
-let d; // 直径
+// let d; // 直径
+let r; // 半径
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -9,22 +10,24 @@ function setup() {
 }
 
 function draw() {
-    d += 2;
+    // 直径
+    // d += 2;
+    r += 4;
 
     // 適当な大きさでreset
     // dist(x1, y1, x2, y2) で (x1, y1)～(x2, y2) までの直線距離を返す。
     // なので、画面中央から画面端 (0, 0) までの最短距離を求めたい場合は上記のように指定すればいい。
-    if(d / 2 > dist(0, 0, width / 2, height / 2)) {
+    if(r > dist(0, 0, width / 2, height / 2)) {
         reset();
     }
 
     clear();
-    circle(width / 2, height / 2, d)
+    circle(width / 2, height / 2, r * 2)
 }
 
 // このメソッドが呼ばれると直径が初期サイズに戻り、リセット
 function reset() {
-    d = 0;
+    r = 0;
 }
 
 // 画面をマウスでクリックするとリセット
