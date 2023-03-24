b6c4647a545863776caa85ac9bef1148b0393be6
みやう <51164077+ryo2141@users.noreply.github.com>
2023-03-25 00:39:12 +0900

    04 constant velocity change (#2)
    * 04 1,2
    
    * 円が画面全体を覆うとリセット
    
    * 半径を持つ
    
    * 色変更
    
    * RGB指定

---
+++ app04.js
@@ -0,0 +1,48 @@
+// let d; // 直径
+// let r; // 半径
+// 直径、背景色、円の色
+let d, bgColor, circleColor;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+
+    // d = 0; // 直径の初期化
+    // RGB色指定
+    circleColor = color(random(255), random(255), random(255));
+    reset();
+}
+
+function draw() {
+    // 直径
+    // d += 2;
+    // r += 4;
+    d += 4;
+
+    // 適当な大きさでreset
+    // dist(x1, y1, x2, y2) で (x1, y1)～(x2, y2) までの直線距離を返す。
+    // なので、画面中央から画面端 (0, 0) までの最短距離を求めたい場合は上記のように指定すればいい。
+    if(d / 2 > dist(0, 0, width / 2, height / 2)) {
+        reset();
+    }
+
+    background(bgColor);
+    fill(circleColor);
+    // clear();
+    circle(width / 2, height / 2, d);
+}
+
+// このメソッドが呼ばれると直径が初期サイズに戻り、リセット
+function reset() {
+    d = 0;
+    bgColor = circleColor;
+    // グレースケール
+    // circleColor = random(255);
+    // RGB色指定
+    circleColor = color(random(255), random(255), random(255));
+}
+
+// 画面をマウスでクリックするとリセット
+// function mouseClicked() {
+//     reset();
+// }
